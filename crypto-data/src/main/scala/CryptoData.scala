774dff5ca222cc9917eb178db8a6c4b231f8f94e
Jakub Cichy <jakub.cichy@softwaremill.com>
2022-07-12 21:51:28 +0200

    Refactor

--- crypto-data/src/main/scala/CryptoData.scala
+++ crypto-data/src/main/scala/CryptoData.scala
@@ -1,6 +1,6 @@
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{Flow, Source}
-import cats.syntax.all._
+import akka.stream.BoundedSourceQueue
+import akka.stream.scaladsl.{Flow, Sink, Source}
 import com.influxdb.client.domain.WritePrecision
 import com.influxdb.client.write.Point
 import io.circe.generic.auto._
@@ -11,12 +11,13 @@ import sttp.client3.akkahttp.AkkaHttpBackend
 import sttp.ws.WebSocketFrame
 
 import java.time.Instant
-import java.util.concurrent.ConcurrentLinkedQueue
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.DurationInt
 import scala.concurrent.{Await, Future}
 
 object CryptoData extends App {
+
+  implicit val as: ActorSystem = ActorSystem("crypto-data")
 
   case class Trade(
       exchange: String,
@@ -29,65 +30,71 @@ object CryptoData extends App {
 
   type Prices = Map[String, Double]
 
-  implicit val as: ActorSystem = ActorSystem("crypto-data")
-
-  val points = new ConcurrentLinkedQueue[Point]()
-
-  val processTrades: AkkaStreams.Pipe[WebSocketFrame.Data[_], WebSocketFrame] =
-    Flow[WebSocketFrame.Data[_]]
-      .collect { case WebSocketFrame.Text(payload, _, _) => decode[Trade](payload) }
-      .collect { case Right(trade) => trade }
-      .map { trade =>
-        Point
-          .measurement("trades")
-          .addTag("base", trade.base)
-          .addTag("direction", trade.direction)
-          .addField("quote", trade.quote)
-          .addField("volume", trade.volume)
-          .time(trade.timestamp, WritePrecision.MS)
-      }
-      .via(Flow[Point].map(points.offer))
-      .map { _ => WebSocketFrame.pong }
-
-  val processPrices: AkkaStreams.Pipe[WebSocketFrame.Data[_], WebSocketFrame] =
-    Flow[WebSocketFrame.Data[_]]
-      .collect { case WebSocketFrame.Text(payload, _, _) => decode[Prices](payload) }
-      .collect { case Right(prices) => prices }
-      .mapConcat { prices =>
-        prices.map {
-          case (currency, price) =>
-            Point
-              .measurement("prices")
-              .addTag("currency", currency)
-              .addField("price", price)
-              .time(Instant.now(), WritePrecision.MS)
-        }.toSeq
-      }
-      .via(Flow[Point].map(points.offer))
-      .map { _ => WebSocketFrame.pong }
+  case class Frame(subscription: String, payload: String)
 
   val subscribeTrades = basicRequest
-    .response(asWebSocketStream(AkkaStreams)(processTrades))
+    .response(asWebSocketStream(AkkaStreams)(processFrame(subscription = "trades")))
     .get(uri"wss://ws.coincap.io/trades/binance")
 
   val subscribePrices = basicRequest
-    .response(asWebSocketStream(AkkaStreams)(processPrices))
+    .response(asWebSocketStream(AkkaStreams)(processFrame(subscription = "prices")))
     .get(uri"wss://ws.coincap.io/prices?assets=ALL")
+
+  def processFrame(subscription: String): AkkaStreams.Pipe[WebSocketFrame.Data[_], WebSocketFrame] =
+    Flow.fromFunction {
+      case WebSocketFrame.Text(payload, _, _) =>
+        framesQueue.offer(Frame(subscription, payload))
+        WebSocketFrame.pong
+      case _ => WebSocketFrame.pong
+    }
+
+  val framesQueue: BoundedSourceQueue[Frame] =
+    Source
+      .queue[Frame](10000)
+      .mapConcat {
+        case Frame("trades", payload) =>
+          decode[Trade](payload) match {
+            case Right(trade) =>
+              val point = Point
+                .measurement("trades")
+                .addTag("base", trade.base)
+                .addTag("direction", trade.direction)
+                .addField("quote", trade.quote)
+                .addField("volume", trade.volume)
+                .time(trade.timestamp, WritePrecision.MS)
+
+              Seq(point)
+            case _ => Seq.empty
+          }
+        case Frame("prices", payload) =>
+          decode[Prices](payload) match {
+            case Right(prices) =>
+              prices.map {
+                case (currency, price) =>
+                  Point
+                    .measurement("prices")
+                    .addTag("currency", currency)
+                    .addField("price", price)
+                    .time(Instant.now(), WritePrecision.MS)
+              }.toSeq
+            case _ => Seq.empty
+          }
+      }
+      .grouped(1000)
+//      .to(Influxdb.write)
+      .to(Sink.foreach[Seq[Point]](x => println(x.size)))
+      .run()
 
   val backend = AkkaHttpBackend.usingActorSystem(as)
 
-  val process = Future {
-    Source.single(Seq.fill(1000)(points.poll)).to(Influxdb.write).run()
-  }
-
-  Future.sequence(
+  val never_ending_story = Future.sequence(
     Seq(
       subscribeTrades.send(backend),
       subscribePrices.send(backend)
     )
   )
 
-  Await.result(process.foreverM, 15.minutes)
+  Await.result(never_ending_story, 15.minutes)
 
   backend.close()
   Influxdb.close()
